c2af511a5c118242b09b12d9d1ba6c9ec15012bb
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-11-04 09:48:54 +0000

    initial commit

---
+++ ClassSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Mathematics";
+let classSchedule = "10:00 AM - 11:30 AM, Mon-Wed-Fri";
+let classroom = "Room 101";
+let classInstructor = "Dr. Smith";
+let studentName = "John Doe";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
